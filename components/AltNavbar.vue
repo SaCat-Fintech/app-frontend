0f17e908f5e4693bdbf41e8efbe7443df22811f3
Cristina <avicon95@protonmail.com>
2023-10-24 20:50:59 -0500

    feat: navbar for login - navbar alt component

---
+++ components/AltNavbar.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="flex w-screen justify-between pt-6 px-10">
+    <div class="hidden md:flex">
+      <NuxtImg class="w-8" src="nav-cat.png" />
+      <p class="mt-2 ml-2">SaCat</p>
+    </div>
+    <p class="mt-2">
+      {{ navQuestion }}
+      <NuxtLink
+        style="
+          color: var(--primary-color);
+          text-decoration: underline;
+          text-underline-offset: 4px;
+        "
+        :to="navRedirect"
+        >{{ navText }}</NuxtLink
+      >
+    </p>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  props: {
+    navQuestion: String,
+    navText: String,
+    navRedirect: String,
+  },
+};
+</script>
+
+<style></style>
--- pages/login.vue
+++ pages/login.vue
@@ -1,20 +1,29 @@
 <template>
-  <body class="flex justify-center items-center h-screen">
-    <div class="text-center flex flex-col">
-      <p class="text-xl sm:text-2xl mb-6">Inicia sesión con tu cuenta</p>
-      <Button
-        icon="pi pi-google"
-        iconPos="left"
-        severity="secondary"
-        class="w-72 sm:w-96 h-12 mb-4"
-        label="Inicia sesión con Google"
+  <div class="h-screen">
+    <nav>
+      <AltNavbar
+        navQuestion="¿No está registrado?"
+        navText="Hágalo aquí"
+        navRedirect="/register"
       />
-      <p class="text-xl sm:text-2xl mb-6">o</p>
-      <InputText class="w-72 sm:w-96 h-12 mb-6" placeholder="Email" />
-      <InputText class="w-72 sm:w-96 h-12 mb-6" placeholder="Contraseña" />
-      <Button class="w-72 sm:w-96 h-12" label="Iniciar sesión" />
+    </nav>
+    <div class="flex justify-center items-center h-5/6">
+      <div class="text-center flex flex-col">
+        <p class="text-xl sm:text-2xl mb-6">Inicia sesión con tu cuenta</p>
+        <Button
+          icon="pi pi-google"
+          iconPos="left"
+          severity="secondary"
+          class="w-72 sm:w-96 h-12 mb-4"
+          label="Inicia sesión con Google"
+        />
+        <p class="text-xl sm:text-2xl mb-4">o</p>
+        <InputText class="w-72 sm:w-96 h-12 mb-6" placeholder="Email" />
+        <InputText class="w-72 sm:w-96 h-12 mb-6" placeholder="Contraseña" />
+        <Button class="w-72 sm:w-96 h-12" label="Iniciar sesión" />
+      </div>
     </div>
-  </body>
+  </div>
 </template>
 
 <script lang="ts"></script>
