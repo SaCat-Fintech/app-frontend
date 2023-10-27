75310715464e695eb84d7ea6cbaf0828e204a7df
Cristina <avicon95@protonmail.com>
2023-10-26 20:59:25 -0500

    chore: validate basic parameters on submission

--- pages/login.vue
+++ pages/login.vue
@@ -8,24 +8,68 @@
       />
     </nav>
     <div class="flex justify-center items-center h-5/6">
-      <div class="text-center flex flex-col">
-        <p class="text-xl sm:text-2xl mb-6">Inicia sesión con tu cuenta</p>
+      <div class="text-center flex flex-col gap-4">
+        <p class="text-xl sm:text-2xl">Inicia sesión con tu cuenta</p>
         <Button
           icon="pi pi-google"
           iconPos="left"
           severity="secondary"
-          class="w-72 sm:w-96 h-12 mb-4"
+          class="w-72 sm:w-96 h-12"
           label="Inicia sesión con Google"
         />
-        <p class="text-xl sm:text-2xl mb-4">o</p>
-        <InputText class="w-72 sm:w-96 h-12 mb-6" placeholder="Email" />
-        <InputText class="w-72 sm:w-96 h-12 mb-6" placeholder="Contraseña" />
-        <Button class="w-72 sm:w-96 h-12" label="Iniciar sesión" />
+        <p class="text-xl sm:text-2xl mb-2">o</p>
+        <form submit="onSubmitForm" class="flex flex-col gap-6">
+          <InputText
+            id="email"
+            v-model="email"
+            class="w-72 sm:w-96 h-12"
+            placeholder="Email"
+          />
+          <InputText
+            id="password"
+            v-model="password"
+            class="w-72 sm:w-96 h-12"
+            placeholder="Contraseña"
+          />
+          <Button
+            @click="onSubmitForm"
+            class="w-72 sm:w-96 h-12"
+            label="Iniciar sesión"
+          />
+        </form>
       </div>
     </div>
   </div>
 </template>
-
-<script lang="ts"></script>
+<script lang="ts">
+export default {
+  data() {
+    return {
+      email: "",
+      password: "",
+    };
+  },
+  computed: {
+    isValidEmail() {
+      // TODO: Prevent SQL injection
+      const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+      return emailPattern.test(this.email);
+    },
+    isValidPassword() {
+      const passwordPattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*\W).{8,}$/;
+      return passwordPattern.test(this.password) && this.password.length >= 8;
+    },
+  },
+  methods: {
+    onSubmitForm() {
+      if (this.isValidEmail && this.isValidPassword) {
+        console.log("SUCCESS");
+      } else {
+        console.log("Not cool");
+      }
+    },
+  },
+};
+</script>
 
 <style></style>
