ab1251482f8abedea297c232c44f1030486b2266
Cristina <avicon95@protonmail.com>
2023-11-05 17:36:16 -0500

    feat: initial information for user session

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   css: [
+    "@/assets/css/main.css",
     "primevue/resources/themes/lara-light-blue/theme.css",
-    "~/assets/css/main.css",
     "primeicons/primeicons.css",
   ],
   postcss: {
